337a5fc89f244687a85b40cca4a3d4b502cfae04
Patrick Conde <patrick.conde.costas@gmail.com>
2018-02-18 15:49:33 -0800

    Background picture

--- Asana API /src/client/app/App.jsx
+++ Asana API /src/client/app/App.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import TaskListContainer from './components/task_list_container';
 
 const App = () => (
-  <div>
-    <p>Hello</p>
+  <div className="overall-app">
     <TaskListContainer />
   </div>
 )
--- Asana API /src/client/app/components/task_list.jsx
+++ Asana API /src/client/app/components/task_list.jsx
@@ -18,16 +18,21 @@ class TaskList extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    // const project = Object.assign({}, this.state)
     this.props.getTasks(this.state["project_id"], this.state["asana_token"])
   }
 
   render () {
     return (
-      <div>
-        <input type="text" onChange={this.handleInput('asana_token')} placeholder="Type in your personal access token"></input>
-        <input type="text" onChange={this.handleInput('project_id')} placeholder="Type in your project id"></input>
-        <button onClick={this.handleSubmit}>Get Tasks</button>
+      <div className="form-div">
+        <p className="form-p">Type in your personal access token:</p>
+        <input className="input-form" type="text"
+          onChange={this.handleInput('asana_token')}
+          placeholder="e.g. 0/ae295cd0c2a56m844e969c4te73z4367"></input>
+        <p className="form-p">Type in your project ID:</p>
+        <input className="input-form" type="text"
+          onChange={this.handleInput('project_id')}
+          placeholder="e.g. 931581952334457"></input>
+        <button className="form-button" onClick={this.handleSubmit}>Get Tasks</button>
       </div>
     )
   }
