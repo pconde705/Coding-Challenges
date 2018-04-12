008c3cf8defb62e29e4a7e367f2aab4d46c78804
Patrick Conde <patrick.conde.costas@gmail.com>
2018-04-11 22:58:18 -0700

    Trying button sequences

--- Game-Othello/src/app/components/board_game.jsx
+++ Game-Othello/src/app/components/board_game.jsx
@@ -11,6 +11,8 @@ class BoardGame extends React.Component {
   colorChange(e) {
     e.preventDefault();
     this.setState({pos: "blue"})
+    console.log(e.currentTarget);
+    e.currentTarget.style["backgroundColor"] == "black" ? e.currentTarget.style = "background-color: white" : e.currentTarget.style = "background-color: black";
   }
 
   render () {
@@ -18,7 +20,10 @@ class BoardGame extends React.Component {
       <div className="overall-div">
         <div className="board">
             <div className="square">
-              <button onClick={this.colorChange}></button>
+              <button className="tile-button" onClick={this.colorChange}></button>
+            </div>
+            <div className="square">
+              <button className="tile-button" onClick={this.colorChange}></button>
             </div>
         </div>
       </div>
