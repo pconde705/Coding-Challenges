30ce651ecfc20125273c9186378021b4c850f057
Patrick Conde <patrick.conde.costas@gmail.com>
2018-04-12 14:15:12 -0700

    Refactored into loops

--- Game-Othello/src/app/components/board.jsx
+++ Game-Othello/src/app/components/board.jsx
@@ -4,7 +4,7 @@ class Board extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = {color: "white"}
+    this.state = {color: "white", whitePoints: 2, blackPoints: 2}
     this.colorChange = this.colorChange.bind(this)
   }
 
@@ -15,349 +15,37 @@ class Board extends React.Component {
   }
 
   render () {
+    let rows = [];
+    for (var i = 0; i < 8; i++) {
+      let rowID = `Row:${i+1}`
+      let cell = []
+      for (var idx = 0; idx < 8; idx++) {
+        let cellID = `Row:${i+1}-Cell:${idx+1}`
+        if ((i == 3 && idx == 3) || (i == 4 && idx == 4)) {
+          cell.push(<td key={cellID} id={cellID}><div className="square">
+                    <button className="tile-button-white" onClick={this.colorChange}></button>
+                    </div></td>)
+        } else if ((i == 3 && idx == 4) || (i == 4 && idx == 3)) {
+          cell.push(<td key={cellID} id={cellID}><div className="square">
+                    <button className="tile-button" onClick={this.colorChange}></button>
+                    </div></td>)
+        } else {
+          cell.push(<td key={cellID} id={cellID}><div className="square">
+                    <button className="no-tile" onClick={this.colorChange}></button>
+                    </div></td>)
+        }
+      }
+      rows.push(<tr key={i} id={rowID}>{cell}</tr>)
+    }
+
     return (
-        <div>
-          <table>
-            <tbody>
-              <tr>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="tile-button-white" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="tile-button" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="tile-button" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="tile-button-white" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-                <td>
-                  <div className="square">
-                    <button className="no-tile" onClick={this.colorChange}></button>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+      <div>
+        <table>
+          <tbody>
+            {rows}
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
