1284ec2024356819f66f272359ca463f64b6e26e
Patrick Conde <patrick.conde.costas@gmail.com>
2018-04-11 18:08:48 -0700

    Trying to implement game logic

--- Game-Othello/src/app/components/board_game.jsx
+++ Game-Othello/src/app/components/board_game.jsx
@@ -2,10 +2,25 @@ import React from 'react'
 
 class BoardGame extends React.Component {
 
+  constructor() {
+    super()
+    this.state = {pos: ""}
+    this.colorChange = this.colorChange.bind(this)
+  }
+
+  colorChange(e) {
+    e.preventDefault();
+    this.setState({pos: "blue"})
+  }
+
   render () {
     return (
-      <div>
-        <h1>Component Success</h1>
+      <div className="overall-div">
+        <div className="board">
+            <div className="square">
+              <button onClick={this.colorChange}></button>
+            </div>
+        </div>
       </div>
     )
   }
